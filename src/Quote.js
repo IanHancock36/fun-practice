3236217c4af3be6878d466528c62950915a4d612
IanHancock36 <northroadian@gmail.com>
2022-05-02 06:44:33 -0500

    swanson quote loaded"

--- src/Quote.js
+++ src/Quote.js
@@ -1,38 +1,24 @@
-import React from 'react'
-import axios from 'axios' 
-const officeUrl =  'https://officeapi.dev/api/quotes' 
+import React from "react";
+import axios from "axios";
+const swansonQuotesUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+
 const Quote = () => {
-    const [post, setPost] = React.useState(null)
-React.useEffect(()=> {
-axios.get(officeUrl).then((response)=>{
-    setPost(response.data)
-    console.log(response.data)
-})
-},[])
-if(!post) return null 
+  const [post, setPost] = React.useState(null);
+  React.useEffect(() => {
+    axios.get(swansonQuotesUrl).then((response) => {
+      setPost(response.data);
+      console.log(response.data);
+    }
+    ).catch(error => {
+      return console.log(error)
+    })
+    ;
+  }, []);
+  if (!post) return null;
 
-return (
-   <div>
+  return <div>{post}</div>;
+};
 
-   </div> 
-) 
-}
+export default Quote;
 
-export default Quote
 
-// const response = await axios.get(officeUrl)
-// console.log(response)
-//   {
-//     "data": {
-//         "_id": "5e9668186a66e65486e244a5",
-//         "content": "Bears, Beets, Battlestar Galactica.",
-//         "character": {
-//           "_id": "5e93b4f03af44260882e33b1",
-//           "firstname": "Jim",
-//           "lastname": "Halpert",
-//           "__v": 0
-//         },
-//         "__v": 0
-//       }
-//    }
-    
