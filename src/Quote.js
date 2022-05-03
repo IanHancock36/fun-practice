d7c7e7cc5cd6c37177579566a18eaeada03e2c74
IanHancock36 <northroadian@gmail.com>
2022-05-03 06:18:26 -0500

    try catch

--- src/Quote.js
+++ src/Quote.js
@@ -1,36 +1,54 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-const swansonQuotesUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+const swansonQuotesUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
 const Quote = () => {
-const [quote ,setQuote] = useState({}) // store the quote from api request
-const [errorMessage , setErrorMessage ] = useState("") // the error message strored in the errorMessage state
-const [loadingQuote, setLoadingQuote] = useState(false) // loading quote state to toggle between loader view and data view
-const [quoteCopied, setQuoteCopied] = useState(false) // after copying quote should show success 
-  
+  const [post, setPost] = useState(false);
+  const [quote, setQuote] = useState({}); // store the quote from api request
+  const [errorMessage, setErrorMessage] = useState(""); // the error message strored in the errorMessage state
+  const [loadingQuote, setLoadingQuote] = useState(false); // loading quote state to toggle between loader view and data view
+  const [quoteCopied, setQuoteCopied] = useState(false); // after copying quote should show success
 
-useEffect(() => {
-    axios.get(swansonQuotesUrl).then((response) => {
-      setPost(response.data);
-      console.log(response.data);
-    }).catch(error => {return console.log(error)});
+  async function fetchRandomQuote() {
+    try {
+      setLoadingQuote(true);
+      setErrorMessage("");
+      setQuoteCopied(false);
+      const quoteObject = await axios.get(
+        "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+      );
+      setQuote(quoteObject.data);
+      setLoadingQuote(false);
+    } catch (error) {
+      setErrorMessage(error.errorMessage);
+      setLoadingQuote(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchRandomQuote();
   }, []);
 
- const newQuote = {
-
- }
   return (
     <div>
-  <div style={{marginTop: 60}}>{post}</div>
-  <div>
-    <button style={{marginTop: 20 , backgroundColor:"blue", color:"white", borderRadius:'22px'}}>New Quote</button> 
+      <div style={{ marginTop: 60 }}>{post}</div>
+      <div>
+        <button
+          style={{
+            marginTop: 20,
+            backgroundColor: "blue",
+            color: "white",
+            borderRadius: "22px",
+          }}
+        >
+          New Quote
+        </button>
+      </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default Quote;
 
-
-// need to have a empty object for the quote 
-  // need to have state 
+// need to have a empty object for the quote
+// need to have state
