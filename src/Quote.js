2748bcacf5700c6b190ec9e8564b321089f8e42f
IanHancock36 <northroadian@gmail.com>
2022-05-03 06:08:19 -0500

    adding state

--- src/Quote.js
+++ src/Quote.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import axios from "axios";
 const swansonQuotesUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
 
 const Quote = () => {
-  const [post, setPost] = React.useState(null);
-  React.useEffect(() => {
+const [quote ,setQuote] = useState({}) // store the quote from api request
+const [errorMessage , setErrorMessage ] = useState("") // the error message strored in the errorMessage state
+const [loadingQuote, setLoadingQuote] = useState(false) // loading quote state to toggle between loader view and data view
+const [quoteCopied, setQuoteCopied] = useState(false) // after copying quote should show success 
+  
+
+useEffect(() => {
     axios.get(swansonQuotesUrl).then((response) => {
       setPost(response.data);
       console.log(response.data);
-    }
-    ).catch(error => {
-      return console.log(error)
-    })
-    ;
+    }).catch(error => {return console.log(error)});
   }, []);
-  if (!post) return null;
 
-  return <div>{post}</div>;
+ const newQuote = {
+
+ }
+  return (
+    <div>
+  <div style={{marginTop: 60}}>{post}</div>
+  <div>
+    <button style={{marginTop: 20 , backgroundColor:"blue", color:"white", borderRadius:'22px'}}>New Quote</button> 
+    </div>
+  </div>
+  )
 };
 
 export default Quote;
 
 
+// need to have a empty object for the quote 
+  // need to have state 
